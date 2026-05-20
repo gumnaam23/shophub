7d2b0f264dbc9cb06e81ea3233fe16a5fa5672b6
Aqib <aqibdev23@gmail.com>
2026-05-20 05:49:12 +0100

    my first

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
   images: {
     domains: ['res.cloudinary.com', 'images.unsplash.com', 'localhost', 'img.clerk.com', "images.pexels.com"],
     remotePatterns: [
